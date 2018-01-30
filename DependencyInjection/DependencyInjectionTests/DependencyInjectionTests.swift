6e1ba53be64fc0138966818fdb1952c5f7753303
htomcat <thamada@yahoo-corp.jp>
2018-01-30 16:12:47 +0900

    [add]Initial commit

---
+++ DependencyInjection/DependencyInjectionTests/DependencyInjectionTests.swift
@@ -0,0 +1,36 @@
+//
+//  DependencyInjectionTests.swift
+//  DependencyInjectionTests
+//
+//  Created by htomcat on 2018/01/30.
+//  Copyright © 2018年 htomcat. All rights reserved.
+//
+
+import XCTest
+@testable import DependencyInjection
+
+class DependencyInjectionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
