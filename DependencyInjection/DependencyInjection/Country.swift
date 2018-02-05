9d595f8347bee7ca594eb90233a88a77b4d970f0
htomcat <thamada@yahoo-corp.jp>
2018-02-05 15:42:09 +0900

    [add]Implement a model

---
+++ DependencyInjection/DependencyInjection/Country.swift
@@ -0,0 +1,16 @@
+//
+//  Country.swift
+//  DependencyInjection
+//
+//  Created by 浜田　智史 on 2018/02/05.
+//  Copyright © 2018年 htomcat. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Country {
+    let name: String
+    let capital: String
+    let color: UIColor
+}
