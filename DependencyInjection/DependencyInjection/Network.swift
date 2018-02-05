b0d13d6e908b73444a8db354b4a17e802a5f94f2
htomcat <thamada@yahoo-corp.jp>
2018-02-05 16:07:02 +0900

    [add]Implement New Class that conforms Networking protocol

---
+++ DependencyInjection/DependencyInjection/Network.swift
@@ -0,0 +1,14 @@
+//
+//  Network.swift
+//  DependencyInjection
+//
+//  Created by htomcat on 2018/02/05.
+//  Copyright © 2018年 htomcat. All rights reserved.
+//
+
+import Foundation
+
+struct Network: Networking {
+    func request(response: (Data?) -> ()) {
+    }
+}
